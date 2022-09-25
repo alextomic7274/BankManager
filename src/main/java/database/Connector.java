c54fe17c176826da52ecdf8808fc07f11f4f8d66
Jan Holtmann <janlh@campus.uni-paderborn.de>
2022-09-25 15:14:44 +0200

    added first draft of a method to generify the execution of SELECT statements

--- src/main/java/database/Connector.java
+++ src/main/java/database/Connector.java
@@ -3,13 +3,39 @@ package database;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.*;
-import java.util.Properties;
+import java.util.*;
 
 public class Connector {
     private Connection con;
 
     public Connector(){
         con = establishConnection();
+    }
+
+    //in development
+    public <T> List<HashMap<String, Object>> executeSelect(LinkedHashMap<String, T> map, String query){
+        List<HashMap<String, Object>> result = new ArrayList<>();
+        try(CallableStatement cstmt = con.prepareCall(query)) {
+            for (Map.Entry<String, T> entry : map.entrySet()) {
+                cstmt.setObject(entry.getKey(), entry.getValue());
+            }
+            try(ResultSet rs = cstmt.executeQuery()){
+                //Convert ResultSet to List
+                ResultSetMetaData rsmd = rs.getMetaData();
+                int columnsNumber = rsmd.getColumnCount();
+                while (rs.next()) {
+                    HashMap<String, Object> row = new HashMap<>();
+                    for (int i = 1; i <= columnsNumber; i++) {
+                        row.put(rsmd.getColumnName(i), rs.getObject(i));
+                    }
+                    result.add(row);
+                }
+            }
+            return result;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     private String getConnLink(){
