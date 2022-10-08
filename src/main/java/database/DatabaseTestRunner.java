95abe098ebe0c6f0ebe182ef7dce9929ddc77254
Jan Holtmann <janlh@campus.uni-paderborn.de>
2022-10-08 18:21:29 +0200

    Added design.txt, which contains the basic database layout

--- src/main/java/database/DatabaseTestRunner.java
+++ src/main/java/database/DatabaseTestRunner.java
@@ -4,6 +4,6 @@ public class DatabaseTestRunner {
 
     public static void main(String[] args) {
         QueryWrapper qw = new QueryWrapper();
-        System.out.println(qw.checkUserLogin("alex7274", "java123"));
+        //you can do stuff here as well
     }
 }
